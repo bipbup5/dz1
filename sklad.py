4da2c1846404edd0029830f4f547e7eced8d57f1
bipbup5 <76110188+bipbup5@users.noreply.github.com>
2021-09-26 19:41:27 +0300

    Update sklad.py

--- sklad.py
+++ sklad.py
@@ -2,7 +2,7 @@ from math import *
 
 a = int(input())
 f = cos((3 - a) / (a ** 2 - 4 * a - 12))
-chislo = int(f * 100000000) % 100000000
+chislo = int(f * (10 ** 8)) % (10 ** 8)
 su = 0
 for elem in str(chislo):
     su += int(elem)
