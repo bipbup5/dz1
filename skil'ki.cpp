ac974fc62b753e7f527b4f003db6db8750c7a817
bipbup5 <76110188+bipbup5@users.noreply.github.com>
2021-09-26 19:39:52 +0300

    Create skil'ki.cpp

---
+++ skil'ki.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n, res = 0, i;
+    std::cin>>n;
+    for (i = pow(10, 7); i < pow(10, 8); i++) {
+        int su = 0, k = i;
+        while (k != 0) {
+            su = su + k % 10;
+            k = k / 10;
+        }
+        if (su == 8) {
+            res++;
+        }
+    }
+    std::cout<<res<<std::endl;
+    return 0;
+}
