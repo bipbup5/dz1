04ab3353153dcd5157ae7ec1416ee2613c3e9a8f
bipbup5 <76110188+bipbup5@users.noreply.github.com>
2021-09-27 21:59:35 +0300

    Update skil'ki.cpp

--- skil'ki.cpp
+++ skil'ki.cpp
@@ -3,16 +3,21 @@
 int main() {
     int n, res = 0, i;
     std::cin>>n;
-    for (i = pow(10, 7); i < pow(10, 8); i++) {
-        int su = 0, k = i;
-        while (k != 0) {
-            su = su + k % 10;
-            k = k / 10;
+    if not (n <= 0 or n >= 73){
+        for (i = pow(10, 7); i < pow(10, 8); i++) {
+            int su = 0, k = i;
+            while (k != 0) {
+                su = su + k % 10;
+                k = k / 10;
+            }
+            if (su == n) {
+                res++;
+            }
         }
-        if (su == n) {
-            res++;
-        }
+        std::cout<<res<<std::endl;
     }
-    std::cout<<res<<std::endl;
+    else{
+        print('impossible')
+    }
     return 0;
 }
